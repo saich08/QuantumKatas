9eee3db26ed448d2c175b1e088e28320387a9956
Ricardo Espinoza <ricardoe@microsoft.com>
2021-11-16 15:17:18 -0800

    Update Katas to a .NET6 based build

--- scripts/Update-QDKVersion.ps1
+++ scripts/Update-QDKVersion.ps1
@@ -18,7 +18,7 @@ param(
         
     .EXAMPLE
     
-        PS> ./Update-QDKVersion.ps1 -Version 0.19.2109165653
+        PS> ./Update-QDKVersion.ps1 -Version 0.20.2111176351-beta
 #>
 
 $katasRoot = Join-Path $PSScriptRoot "\..\"
